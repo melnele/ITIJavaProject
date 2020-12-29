6b33047222581c2bae7a6aef1d52aa59dbd551af
Mohamed Elneily <mohamed.nele@gmail.com>
2020-12-29 21:59:31 +0200

    Server created and ui changed to work with it

---
+++ Server/src/server/DBConnection.java
@@ -0,0 +1,35 @@
+package server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
+/**
+ *
+ * @author AhmedG
+ */
+public class DBConnection {
+
+    private static final String DB_URL = "jdbc:derby://localhost:1527/XOGame";
+    private static final String DB_USERNAME = "XOGame";
+    private static final String DB_PASSWORD = "12345";
+
+    public static Connection connectdb() {
+        try {
+            DriverManager.registerDriver(new ClientDriver());
+            Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connecting.............");
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("There was an Error connecting to the DB.");
+        }
+        return null;
+    }
+}
